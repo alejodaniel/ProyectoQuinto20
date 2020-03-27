2171c1ff0e381180685c1defc1ffbd27ec94a0cb
ErickErraez <erickerraez2@gmail.com>
2020-03-27 13:13:15 -0500

    Modo Oscuro

--- BiometricoMovil/src/app/app.component.ts
+++ BiometricoMovil/src/app/app.component.ts
@@ -1,27 +1,27 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
-import { Platform } from '@ionic/angular';
-import { SplashScreen } from '@ionic-native/splash-screen/ngx';
-import { StatusBar } from '@ionic-native/status-bar/ngx';
+import {Platform} from '@ionic/angular';
+import {SplashScreen} from '@ionic-native/splash-screen/ngx';
+import {StatusBar} from '@ionic-native/status-bar/ngx';
+import {ThemeService} from './services/theme.service';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: 'app.component.html',
-  styleUrls: ['app.component.scss']
+    selector: 'app-root',
+    templateUrl: 'app.component.html',
+    styleUrls: ['app.component.scss']
 })
 export class AppComponent {
-  constructor(
-    private platform: Platform,
-    private splashScreen: SplashScreen,
-    private statusBar: StatusBar
-  ) {
-    this.initializeApp();
-  }
 
-  initializeApp() {
-    this.platform.ready().then(() => {
-      this.statusBar.styleDefault();
-      this.splashScreen.hide();
-    });
-  }
+    constructor(private platform: Platform, private splashScreen: SplashScreen, private statusBar: StatusBar,
+                private themeService: ThemeService) {
+        this.initializeApp();
+
+    }
+
+    initializeApp() {
+        this.platform.ready().then(() => {
+            this.statusBar.styleDefault();
+            this.splashScreen.hide();
+        });
+    }
 }
--- BiometricoMovil/src/app/tab3/tab3.page.ts
+++ BiometricoMovil/src/app/tab3/tab3.page.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {ThemeService} from '../services/theme.service';
 
 @Component({
-  selector: 'app-tab3',
-  templateUrl: 'tab3.page.html',
-  styleUrls: ['tab3.page.scss']
+    selector: 'app-tab3',
+    templateUrl: 'tab3.page.html',
+    styleUrls: ['tab3.page.scss']
 })
 export class Tab3Page {
 
-  constructor() {}
+    darkMode = this.themeService.themeDark;
+
+    constructor(private themeService: ThemeService) {
+
+    }
+
+
+    changeTheme() {
+        this.themeService.changeTheme(this.darkMode);
+        this.darkMode = this.themeService.themeDark;
+    }
 
 }
