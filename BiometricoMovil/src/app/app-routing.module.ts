35c225bc32ffc290d6d437a75b8fdc1561fac613
alejodaniel <adf.coronel@yavirac.edu.ec>
2020-03-27 13:39:05 -0500

    'bio'

---
+++ BiometricoMovil/src/app/app-routing.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () =>  import('./tabs/tabs.module').then(m => m.TabsPageModule)
+  },
+
+  {
+    path: 'registro',
+    loadChildren: () => import('./pages/registro/registro.module').then( m => m.RegistroPageModule)
+  },
+  {
+    path: 'ubicacion',
+    loadChildren: () => import('./pages/ubicacion/ubicacion.module').then( m => m.UbicacionPageModule)
+  },
+];
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
---
+++ BiometricoMovil/src/app/pages/ubicacion/ubicacion.page.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+
+
+declare var google;
+@Component({
+  selector: 'app-ubicacion',
+  templateUrl: './ubicacion.page.html',
+  styleUrls: ['./ubicacion.page.scss'],
+})
+export class UbicacionPage implements OnInit {
+
+  constructor( ) { }
+
+  ngOnInit() {
+  }
+
+}
